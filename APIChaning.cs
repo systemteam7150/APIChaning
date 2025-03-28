a641f402e6ab61117ef7ba39c3d4960549ac6cb3
DmytroS <systemteam7150@gmail.com>
2025-03-28 09:46:40 -0400

    Initial commit

---
+++ APIChaning.cs
@@ -0,0 +1,50 @@
+﻿using RestSharp;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        var client = new RestClient("https://petstore.swagger.io/v2");
+
+        // Create a new pet "Fluffy"
+        var createRequest = new RestRequest("/pet", Method.Post);
+        var petData = new
+        {
+            id = 28041981, // Unique ID
+            name = "Fluffy",
+            status = "available"
+        };
+        createRequest.AddJsonBody(petData);
+        var createResponse = client.Execute(createRequest);
+        Debug.WriteLine($"Pet Created: {createResponse.Content}");
+
+        // Add a short delay to allow Petstore to process creation
+        Thread.Sleep(2000);
+
+        // Retrieve the created pet
+        var petId = petData.id;
+        var getRequest = new RestRequest($"/pet/{petId}", Method.Get);
+        var getResponse = client.Execute(getRequest);
+        Debug.WriteLine($"Retrieved Pet: {getResponse.Content}");
+
+        // Step 3: Update "Fluffy" to "Fuzzy"
+        var updateRequest = new RestRequest("/pet", Method.Put);
+        var updatedPetData = new
+        {
+            id = petId,
+            name = "Fuzzy",
+            status = "available"
+        };
+        updateRequest.AddJsonBody(updatedPetData);
+        var updateResponse = client.Execute(updateRequest);
+        Debug.WriteLine($"Updated Pet: {updateResponse.Content}");
+
+        // Delete "Fuzzy"
+        var deleteRequest = new RestRequest($"/pet/{petId}", Method.Delete);
+        var deleteResponse = client.Execute(deleteRequest);
+        Debug.WriteLine($"Deleted Pet: {deleteResponse.Content}");
+    }
+}
